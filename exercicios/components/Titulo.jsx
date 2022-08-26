7ee6c806ebf826f4f78631b2acfab17a1ca67e77
José Wesley Gonçalves <wesleymagnos@hotmail.com>
2022-08-26 18:22:24 -0300

    init about components

---
+++ exercicios/components/Titulo.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Titulo() {
+    return (
+        <>
+            <h1>Titulo</h1>
+            <h1>subtitulo</h1>
+        </>
+    )
+}
---
+++ exercicios/pages/basicos/usandoTitulo.jsx
@@ -0,0 +1,14 @@
+
+import Titulo from "../../components/Titulo"
+
+export default function usandoTitulo() {
+    return (
+        <div>
+            <Titulo
+                principal="Pagina de cadastro"
+                secundario="Incluir, alterar e excluir coisas"
+            />
+            <h1>Usando Título</h1>
+        </div>
+    )
+}
