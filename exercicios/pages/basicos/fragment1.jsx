0a32942275b3196f5e3a004e83466039e6904577
José Wesley Gonçalves <wesleymagnos@hotmail.com>
2022-08-26 18:19:49 -0300

    fragments example

---
+++ exercicios/pages/basicos/fragment1.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function fragment() {
+    return (
+        <React.Fragment>
+            <h1>Titulo</h1>
+            <h1>Fragment</h1>
+        </React.Fragment>
+    )
+}
