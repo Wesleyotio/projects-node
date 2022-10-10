5f85f5700f590492b63c6a0752f76a0d2060dd87
José Wesley Gonçalves <wesleymagnos@hotmail.com>
2022-10-10 12:27:03 -0300

    component door updated

--- portas/components/Porta.jsx
+++ portas/components/Porta.jsx
@@ -2,8 +2,14 @@ import styles from "../styles/Porta.module.css";
 
 export default function Porta(props) {
     return (
-        <div className={styles.porta}>
-            <div className={styles.numero}>3</div>
+        <div className={styles.area}>
+            <div className={`${styles.estrutura} ${styles.selecionada}`}>
+                <div className={styles.porta}>
+                    <div className={styles.numero}>3</div>
+                    <div className={styles.macaneta}></div>
+                </div>
+            </div>
+            <div className={styles.chao}></div>
         </div>
     )
 }
