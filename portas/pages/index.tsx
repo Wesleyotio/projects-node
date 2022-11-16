31c591361f51aea913ba756b17a0787cd9b50742
José Wesley <Wesleymagnos@gmail.com>
2022-11-16 17:55:11 -0300

    update pages/routes

--- portas/pages/index.tsx
+++ portas/pages/index.tsx
@@ -1,14 +1,36 @@
+import styles from "../styles/Formulario.module.css"
+import Cartao from "../components/Cartao";
+import Link from "next/link";
+import Entradanumerica from "../components/EntradaNumerica";
 import { useState } from "react";
-import Porta from "../components/Porta";
-import PortaModel from "../model/porta";
 
-export default function Home() {
-  const [p1, setP1] =  useState(new PortaModel(1))
-
-
+export default function Formulario() {
+  const [qtdePortas, setQtdePortas] = useState(3)
+  const [comPresente, setComPresente] = useState(1)
   return (
-    <div style={{ display: "flex" }}>
-      <Porta value={p1} onChange={novaPorta => setP1(novaPorta)} />
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c"> 
+          <h1>Monty Hall</h1>
+        </Cartao>
+        <Cartao>
+          <Entradanumerica text="Qtde Portas" 
+          value={comPresente} onChange={novaPortacomPresente => setComPresente(novaPortacomPresente) }/>
+        </Cartao>
+
+      </div>
+      <div>
+        <Cartao>
+          <Entradanumerica text="Porta com Presente?" 
+            value={qtdePortas} onChange={novaQtde => setQtdePortas(novaQtde) }/>
+        </Cartao>
+        <Cartao bgcolor="#28a085">
+        <Link href={`/jogo/${qtdePortas}/${comPresente}`}>
+            <h2 className={styles.link}>Iniciar</h2>
+        </Link>
+        </Cartao>
+
+      </div>
     </div>
   )
 }
