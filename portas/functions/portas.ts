6e5e640242861cbb63d030a92585badc2734f324
José Wesley <Wesleymagnos@gmail.com>
2022-11-16 17:56:05 -0300

    using typescript

--- portas/functions/portas.ts
+++ portas/functions/portas.ts
@@ -1,9 +1,20 @@
 import PortaModel from "../model/porta";
 
-export function criarPortas(qtde: number, selecionada number): PortaModel[] {
-    return Array.from({length:10}, (_, i) => {
+export function criarPortas(qtde: number, portacomPresente: number): PortaModel[] {
+    return Array.from({length: qtde}, (_, i) => {
         const numero = i + 1
-        const temPresente = numero == selecionada
+        const temPresente = numero === portacomPresente
         return new PortaModel(numero, temPresente)
+    })
+}
+
+export function atualizarPortas(portas: PortaModel[], portaModificada: PortaModel ) {
+    return portas.map(portaAtual => {
+        const igualModificada = portaAtual.numero === portaModificada.numero
+        if (igualModificada) {
+            return portaModificada
+        }else {
+            return portaModificada.aberta ? portaAtual :  portaAtual.desselecionar()
+        }
     })
 }
